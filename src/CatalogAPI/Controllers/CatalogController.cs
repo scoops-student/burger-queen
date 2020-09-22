8ea77a62e603b2939bac2cc2f4d52487841b0061
Hannah Kiekens <hannah.kiekens@scoops.com>
2020-09-22 11:21:24 +0200

    Scaffold project

---
+++ src/CatalogAPI/Controllers/CatalogController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CatalogAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(ILogger<CatalogController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
---
+++ src/CatalogAPI/Infrastructure/CatalogContextHelper.cs
@@ -0,0 +1,28 @@
+﻿using CatalogAPI.Model;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogAPI.Infrastructure
+{
+    public class CatalogContextHelper
+    {
+        public async Task SeedFromFilesAsync(CatalogContext context, IWebHostEnvironment env, ILogger<CatalogContextHelper> logger)
+        {
+            var contentRootPath = env.ContentRootPath;
+        }
+
+        private IEnumerable<CatalogType> GetCatalogTypesFromFile(string contentRootPath, ILogger<CatalogContextHelper> logger)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<CatalogItem> GetCatalogItemsFromFile(string contentRootPath, ILogger<CatalogContextHelper> logger)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
