9a85f6243572a7a9c98d35629f057ce2e9e7467a
Femke Vermander <femke.vermander@student.vives.be>
2020-09-23 16:19:35 +0200

    Feature/document-routes/AlleHttp

--- src/CatalogAPI/Controllers/CatalogController.cs
+++ src/CatalogAPI/Controllers/CatalogController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using CatalogAPI.Model;
 using Microsoft.AspNetCore.Http;
@@ -21,6 +23,7 @@ namespace CatalogAPI.Controllers
             _logger = logger;
         }
 
+        //CatalogTitem
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -30,17 +33,92 @@ namespace CatalogAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable< CatalogItem>>> GetAllCatalogItems()
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<CatalogItem>>> GetAllCatalogItems()
         {
             throw new NotImplementedException();
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<CatalogItem>> GetCatalogItemById(long id)
         {
             throw new NotImplementedException();
         }
 
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<CatalogItem>> DeleteCatalogItemById(long id)
+        {
+            throw new NotImplementedException();
+        }
 
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<CatalogItem>>> DeleteAllCatalogItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPatch("{id}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<CatalogItem>> UpdateCatalogItemById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        //CatalogItem
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<CatalogType>> CreateCatalogType(CatalogType catalogtype)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<CatalogType>>> GetAllCatalogType()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<CatalogType>> GetCatalogTypeById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<CatalogType>> DeleteCatalogTypeById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<CatalogType>>> DeleteAllCatalogTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPatch("{id}")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<CatalogType>> UpdateCatalogTypeById(long id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/CatalogAPI/Startup.cs
+++ src/CatalogAPI/Startup.cs
@@ -55,6 +55,7 @@ namespace CatalogAPI
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                //c.RoutePrefix = string.Empty;
             });
 
             app.UseRouting();
