7783fea87b31fe0dd9d6b440c602e987ef147d88
Hannah Kiekens <hannah.kiekens@scoops.com>
2020-09-23 15:47:29 +0200

    Simple matblazor UI

--- src/CatalogUI/Startup.cs
+++ src/CatalogUI/Startup.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CatalogUI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +27,6 @@ namespace CatalogUI
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
