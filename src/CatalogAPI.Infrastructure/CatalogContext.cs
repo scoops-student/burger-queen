a2a1a560f4d60c42f72fb94cd529992c184f72d3
AybIbourt <60689457+AybIbourt@users.noreply.github.com>
2020-10-21 10:11:03 +0200

    Load Data from Files (#42)

--- src/CatalogAPI.Infrastructure/CatalogContext.cs
+++ src/CatalogAPI.Infrastructure/CatalogContext.cs
@@ -1,25 +1,40 @@
-﻿using CatalogAPI.Domain.Models;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using CatalogAPI.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CatalogAPI.Infrastructure
 {
     public class CatalogContext : DbContext
-    { 
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options)
+            : base(options)
+        {
+        }
 
         // Create tables for the database. Tables have the same name as the DbSet and they're plural
         public DbSet<CatalogItem> CatalogItems { get; set; }
+
         public DbSet<CatalogType> CatalogTypes { get; set; }
 
-        //Set table names singular
+        // Set table names singular
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<CatalogType>().ToTable("CatalogType");
             modelBuilder.Entity<CatalogItem>().ToTable("CatalogItem");
+
+            modelBuilder.Entity<CatalogType>().Property(c => c.Id);
+            modelBuilder.Entity<CatalogType>().Property(c => c.Type).IsRequired();
+            //modelBuilder.Entity<CatalogType>().Property(c => c.CatalogItem);
+
+            modelBuilder.Entity<CatalogItem>().Property(c => c.Id);
+            modelBuilder.Entity<CatalogItem>().Property(c => c.Name).IsRequired();
+            modelBuilder.Entity<CatalogItem>().Property(c => c.Description);
+            modelBuilder.Entity<CatalogItem>().Property(c => c.Price);
+            modelBuilder.Entity<CatalogItem>().Property(c => c.PictureUri);
+            modelBuilder.Entity<CatalogItem>().Property(c => c.CatalogTypeId);
         }
     }
 }
---
+++ src/CatalogAPI.Infrastructure/Migrations/20201015132814_IntitialCatalog.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CatalogAPI.Infrastructure.Migrations
+{
+    public partial class IntitialCatalog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CatalogType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatalogItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    PictureUri = table.Column<string>(nullable: true),
+                    CatalogTypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CatalogItem_CatalogType_CatalogTypeId",
+                        column: x => x.CatalogTypeId,
+                        principalTable: "CatalogType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatalogItem_CatalogTypeId",
+                table: "CatalogItem",
+                column: "CatalogTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CatalogItem");
+
+            migrationBuilder.DropTable(
+                name: "CatalogType");
+        }
+    }
+}
