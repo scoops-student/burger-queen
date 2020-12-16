4e6b31900291add5d8607a5d8420812cb9f0f3bf
AybIbourt <60689457+AybIbourt@users.noreply.github.com>
2020-12-16 15:01:36 +0100

    FilesController

--- src/AdminUiWasm/Program.cs
+++ src/AdminUiWasm/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Tewr.Blazor.FileReader;
 
 namespace AdminUiWasm
 {
@@ -16,6 +17,11 @@ namespace AdminUiWasm
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddFileReaderService(options =>
+            {
+                options.UseWasmSharedBuffer = true;
+            });
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress), });
             builder.Services.AddTransient(sp => new CatalogApi.catalog_apiClient("https://localhost:5001", sp.GetService<HttpClient>()));
